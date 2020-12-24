c5114cf77236fabedc72a989782d2ca6be6a4b0c
yqm <1024744235@qq.com>
2020-12-24 22:28:09 +0800

    详情列表待添加

--- src/config/mock/pannelData.js
+++ src/config/mock/pannelData.js
@@ -1,11 +1,184 @@
 const pannelData = [
-  {
-    id: 1,
-    name: "小王子",
-    author: "霍华德",
-    publisher: "清华大学出版社",
-    cover:'http://img3m7.ddimg.cn/26/3/29165327-1_l_13.jpg'
-  }
+  [{
+    id:"1",
+    name:"欢喜：冯唐时间管理手账",
+    author:"冯唐",
+    publisher:"北京联合出版有限公司",
+    place:"图书馆二楼南①区 14排A面1列1层",
+    state:"10",
+    cover:"http://img3m5.ddimg.cn/37/6/28983475-5_u_10.jpg"
+  }],
+  [{
+    id:"2",
+    name:"灵光集：兰波诗歌集注",
+    author:"阿蒂尔·兰波",
+    publisher:"商务印书馆",
+    place:"图书馆二楼南①区 12排A面2列5层",
+    state:"8",
+    cover:"http://img3m8.ddimg.cn/70/3/29133988-5_u_3.jpg"
+  }],
+  [{
+    id:"3",
+    name:"卡梅里亚的哲学世界",
+    author:"胡安·安东尼奥·里维拉",
+    publisher:"海南出版社",
+    place:"图书馆二楼南①区 11排A面3列5层",
+    state:"5",
+    cover:"http://img3m0.ddimg.cn/83/24/29140040-7_u_3.jpg"
+  }],
+  [{
+    id:"4",
+    name:"古代人的日常生活",
+    author:"讲历史的王老师",
+    publisher:"江苏凤凰文艺出版社",
+    place:"图书馆二楼西①区 8排A面3列5层",
+    state:"12",
+    cover:"http://img3m2.ddimg.cn/14/2/28509242-4_u_6.jpg"
+  }],
+  [{
+    id:"5",
+    name:"书信里的逝水年华—钱锺书与我",
+    author:"许渊冲",
+    publisher:"江西美术出版社",
+    place:"图书馆二楼南①区 2排B面2列5层",
+    state:"1",
+    cover:"http://img3m8.ddimg.cn/84/16/29137368-5_u_7.jpg"
+  }],
+  [{
+    id:"6",
+    name:"漫画诺贝尔科学家：物理学奖",
+    author:"胖乐胖乐",
+    publisher:"湖南科技出版社",
+    place:"图书馆二楼北①区 7排B面1列3层",
+    state:"3",
+    cover:"http://img3m2.ddimg.cn/42/9/29136732-5_u_3.jpg"
+  }],
+  [{
+    id:"7",
+    name:"星光的速度",
+    author:"科林·斯图尔特 提莫·阿巴迪亚",
+    publisher:"辽宁少年儿童出版社",
+    place:"图书馆二楼西①区 11排B面6列5层",
+    state:"2",
+    cover:"http://img3m2.ddimg.cn/52/20/28517992-5_u_2.jpg"
+  }],
+  [{
+    id:"8",
+    name:"众神的战争:希腊神话与西方艺术",
+    author:"江逐浪",
+    publisher:"化学工业出版社",
+    place:"图书馆二楼北①区 11排A面3列5层",
+    state:"4",
+    cover:"http://img3m3.ddimg.cn/66/19/29150913-5_u_13.jpg"
+  }],
+  [{
+    id:"9",
+    name:"浮生六记",
+    author:"沈复",
+    publisher:"时代文艺出版社",
+    place:"图书馆二楼东①区 3排B面12列1层",
+    state:"6",
+    cover:"http://img3m4.ddimg.cn/46/23/26487694-5_u_3.jpg"
+  }],
+  [{
+    id:"10",
+    name:"文豪日历2021",
+    author:"人民文学出版社外国文学编辑室",
+    publisher:"人民文学出版社",
+    place:"图书馆二楼北①区 1排A面1列3层",
+    state:"5",
+    cover:"http://img3m4.ddimg.cn/48/32/28991604-4_u_3.jpg"
+  }],
+  [{
+    id:"11",
+    name:"掬水月在手：镜中的叶嘉莹",
+    author:"行人文化 活字文化",
+    publisher:"四川人民出版社",
+    place:"图书馆二楼北①区 7排B面4列2层",
+    state:"2",
+    cover:"http://img3m5.ddimg.cn/3/22/29137485-5_u_9.jpg"
+  }],
+  [{
+    id:"12",
+    name:"烟火漫卷",
+    author:"迟子建",
+    publisher:"人民文学出版社",
+    place:"图书馆二楼南①区 7排B面2列1层",
+    state:"8",
+    cover:"http://img3m3.ddimg.cn/7/6/29124223-3_u_3.jpg"
+  }],
+  [{
+    id:"13",
+    name:"趣说中国史",
+    author:"趣哥",
+    publisher:"台海出版社",
+    place:"图书馆二楼南①区 11排A面12列2层",
+    state:"6",
+    cover:"http://img3m1.ddimg.cn/98/3/29151341-8_u_6.jpg"
+  }],
+  [{
+    id:"14",
+    name:"半小时漫画经济学4：理财篇",
+    author:"陈磊 半小时漫画团队",
+    publisher:"海南出版社",
+    place:"图书馆二楼西①区 5排A面7列4层",
+    state:"7",
+    cover:"http://img3m0.ddimg.cn/25/13/29128300-5_u_3.jpg"
+  }],
+  [{
+    id:"15",
+    name:"培养孩子的社会情商",
+    author:"叶如风",
+    publisher:"中国妇女出版社",
+    place:"图书馆二楼西①区 7排B面7列4层",
+    state:"2",
+    cover:"http://img3m3.ddimg.cn/56/34/29140013-3_u_6.jpg"
+  }],
+  [{
+    id:"16",
+    name:"永远不要停下前进的脚步",
+    author:"石雷鹏",
+    publisher:"天地出版社",
+    place:"图书馆二楼南①区 3排A面3列3层",
+    state:"3",
+    cover:"http://img3m1.ddimg.cn/61/21/29145661-7_u_9.jpg"
+  }],
+  [{
+    id:"17",
+    name:"你想过怎样的一生",
+    author:"海克·法勒",
+    publisher:"北京联合出版有限公司",
+    place:"图书馆二楼北①区 11排B面6列3层",
+    state:"5",
+    cover:"http://img3m0.ddimg.cn/55/35/29133280-4_u_9.jpg"
+  }],
+  [{
+    id:"18",
+    name:"人生随时可以重来",
+    author:"摩西奶奶",
+    publisher:"北京日报出版社（原同心出版社）",
+    place:"图书馆二楼西①区 6排B面11列1层",
+    state:"1",
+    cover:"http://img3m5.ddimg.cn/63/14/28978155-8_u_3.jpg"
+  }],
+  [{
+    id:"19",
+    name:"回归故里",
+    author:"迪迪埃·埃里蓬",
+    publisher:"上海文化出版社",
+    place:"图书馆二楼北①区 2排A面11列3层",
+    state:"2",
+    cover:"http://img3m3.ddimg.cn/28/11/28559053-6_u_17.jpg"
+  }],
+  [{
+    id:"20",
+    name:"董其昌传",
+    author:"孙炜",
+    publisher:"广西师范大学出版社",
+    place:"图书馆二楼南①区 11排B面4列4层",
+    state:"4",
+    cover:"http://img3m7.ddimg.cn/11/26/29114327-8_u_5.jpg"
+  }]
 ]
 
 export default pannelData
--- src/pages/search/index.js
+++ src/pages/search/index.js
@@ -15,14 +15,12 @@ import CardActionArea from '@material-ui/core/CardActionArea'
 import CardContent from '@material-ui/core/CardContent'
 import CloseIcon from '@material-ui/icons/Close'
 import CardMedia from '@material-ui/core/CardMedia'
-import ArrowDropDownCircleIcon from '@material-ui/icons/ArrowDropDownCircle'
 import Typography from '@material-ui/core/Typography'
 import {actionCreators as frameac} from '../container/store'
 import {actionCreators as borrowac} from '../borrow/store'
-import Axios from '../../utils/request'
-import Token from '../../utils/token'
-import './style.styl'
 import SearchTab from "../../components/searchTab";
+import pannelData from '../../config/mock/pannelData'
+import './style.styl'
 
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -57,9 +55,6 @@ const useStyles = makeStyles((theme) => ({
 
 const Search = (props) => {
   const classes = useStyles()
-  // const [timeSort, setTimeSort] = useState(0) //0表示不实用，1正序，-1倒序
-  // const [AlphaSort, setAlphaSort] = useState(0)
-  const [sort, setSort] = useState([null, null])
   const [bookData, setBookData] = useState(books)
   const [perpage, setPerpage] = useState(20)
   const [perrow, setPerrow] = useState(5)
@@ -79,102 +74,56 @@ const Search = (props) => {
   const [layers, setLayers] = useState([]) //表示可以选择的层范围
   const [showPannel, setShowPannel] = useState(-1)
   const [borrowingBooks, setBorrowingBooks] = useState([])
-  const [functionButtonStyle, setFunctionButtonStyle] = useState(0)
+  const [functionButtonStyle, setFunctionButtonStyle] = useState(1)
   const [visible, setVisible] = useState(false)
-  const [pannelData, setPannelData] = useState({
-    author: '',
-    cover: '',
-    entry_time: '',
-    id: '',
-    name: '',
-    place: '',
-    price: 0.0,
-    publisher: '',
-    state: 0,
-    summary: '',
-    borrowed: 0,
-    title: '',
-    version: '', //书本第几版
-  })
   let {login} = props //登陆的标志符
 
   //获取book的数据，直接返回
 
   //如果login状态发生变化，就要重新查看是否有进入usercenter的权限
-  useEffect(() => {
-    console.log(login)
-    if (login || Token.validate()) {
-      console.log('验证通过')
-    } else {
-      setBorrowingBooks([])
-      setFunctionButtonStyle(-3)
-    }
-  }, [login])
+  // useEffect(() => {
+  //   console.log(login)
+  //   if (login || Token.validate()) {
+  //     console.log('验证通过')
+  //   } else {
+  //     setBorrowingBooks([])
+  //     setFunctionButtonStyle(-3)
+  //   }
+  // }, [login])
 
-  //第一次加载时获取所有的品类数据，不带上任何搜索，获取所有的类别数据
-  useEffect(() => {
-    Axios.post('/api/book/getSearchTypes', {}).then((res) => {
-      if (res.result === 1) {
-        setScreen(res.data)
-      }
-    })
-  }, [])
 
   //获取用户数据和借书的情况
-  const getInitUserData = () => {
-    Axios.post('/api/user/getUserData', {}).then((res) => {
-      if (res.result === 1) {
-        props.modifyUserInfo({
-          id: res.data.id,
-          card: res.data.card,
-          name: res.data.name,
-          cover: res.data.cover,
-          identity: res.data.identity,
-          hasBorrowed: res.data.hasBorrowed,
-          isBorrowing: res.data.isBorrowing,
-        })
-      } else {
-        props.modifyShowAlert(true, '获取您的信息失败', 'error')
-      }
-    })
-    Axios.post('/api/user/getUserIsBorrowingBook', {}).then((res) => {
-      if (res.result === 1) {
-        setBorrowingBooks(res.data)
-      } else {
-        props.modifyShowAlert(true, '获取您的信息失败', 'error')
-      }
-    })
-  }
 
-  //验证是否登录
-  useEffect(() => {
-    console.log(props.login)
-    //如果内存里登录成功或本地token依然有效，那么数据也肯定加载进来了
-    if (props.login || Token.validate()) {
-      // props.modifyLogin(true)
-      getInitUserData()
-    } else {
-      console.log('第三方登录验证')
-      //没有登陆成功，查看是否是通过第三方登陆进来的
-      if (props.location.search !== '') {
-        //说明是由第三方登录进来的，这个链接只有可能是通过第三方登录进来的
-        let token_array = props.location.search
-          .split('?')[1]
-          .split('&')[0]
-          .split('=')
-        console.log(token_array)
-        //如果有token这个字段，说明第三方登陆
-        if (token_array[0] === 'token') {
-          //本地设置token
-          Token.set(token_array[1])
-          //然后就可以获取数据了
-          getInitUserData(token_array[1])
-        }
-      } else {
-
-      }
-    }
-  }, [])
+  //
+  // //验证是否登录
+  // useEffect(() => {
+  //   console.log(props.login)
+  //   //如果内存里登录成功或本地token依然有效，那么数据也肯定加载进来了
+  //   if (props.login || Token.validate()) {
+  //     // props.modifyLogin(true)
+  //     getInitUserData()
+  //   } else {
+  //     console.log('第三方登录验证')
+  //     //没有登陆成功，查看是否是通过第三方登陆进来的
+  //     if (props.location.search !== '') {
+  //       //说明是由第三方登录进来的，这个链接只有可能是通过第三方登录进来的
+  //       let token_array = props.location.search
+  //         .split('?')[1]
+  //         .split('&')[0]
+  //         .split('=')
+  //       console.log(token_array)
+  //       //如果有token这个字段，说明第三方登陆
+  //       if (token_array[0] === 'token') {
+  //         //本地设置token
+  //         Token.set(token_array[1])
+  //         //然后就可以获取数据了
+  //         getInitUserData(token_array[1])
+  //       }
+  //     } else {
+  //
+  //     }
+  //   }
+  // }, [])
 
   //修改当前所在的图书馆
   const handleChangeLibrary = (value) => {
@@ -188,15 +137,7 @@ const Search = (props) => {
   }
 
   //展示pannel
-  const showSummary = (index, id) => {
-    Axios.post('/api/book/searchBookData', {
-      bookId: id,
-    }).then((res) => {
-      if (res.result === 1) {
-        setPannelData(res.data)
-
-      }
-    })
+  const showSummary = (index) => {
     setShowPannel(index)
   }
 
@@ -282,7 +223,6 @@ const Search = (props) => {
         <div className="searchResult">
           {
             bookData.map((item, index) => {
-              console.log(bookData)
               return (
                 <div className="searchLine" key={index}>
                   {item.map((item2) => {
@@ -329,38 +269,51 @@ const Search = (props) => {
                   {showPannel === index ? (
                     <div className="searchSummary">
                       <div className="closeButton" onClick={() => hideSummary()}>
-                        <CloseIcon/>
+                        <CloseIcon width='20px' height='20px' style={{width: '20px', height: '20px'}}/>
                       </div>
                       <div className="searchMainData">
-                        <div className="searchBookName">{pannelData.name}</div>
+                        <div className="searchBookName">{pannelData[index][0].name}</div>
                         <div className="searchBookItem">
                           <span className="searchBookBefore">作者</span> |{' '}
-                          {pannelData.author}
+                          {pannelData[index][0].author}
                         </div>
                         <div className="searchBookItem">
                           <span className="searchBookBefore">出版社</span> |{' '}
-                          {pannelData.publisher}
+                          {pannelData[index][0].publisher}
                         </div>
                         <div className="searchBookItem">
                           <span className="searchBookBefore">书籍位置</span> |{' '}
-                          {pannelData.place}
+                          {pannelData[index][0].place}
                         </div>
                         <div className="searchBookItem">
                           <span className="searchBookBefore">剩余可借</span> |{' '}
-                          {pannelData.state}
+                          {pannelData[index][0].state}
                         </div>
                       </div>
                       <div className="searchBookMore">
-                        <img src={pannelData.cover} className="searchBookImg"/>
+                        <img src={pannelData[index][0].cover} className="searchBookImg"/>
+                        <h3 className='book-detail-name'
+                            title={pannelData[index][0].name}>{pannelData[index][0].name}</h3>
                         {
                           //0代表取消加入
                           functionButtonStyle === 0 ?
                             (
-                              <Button
-                                color="secondary"
-                                onClick={() => cancelBorrowBook(pannelData.id)}>
-                                不加入预借书单
-                              </Button>
+                              <div className='book-detail-btn-wrapper'>
+                                <Button
+                                  size="small"
+                                  className="positiveButton"
+                                  variant="outlined"
+                                  color="secondary"
+                                  onClick={() => cancelBorrowBook(pannelData[index][0].id)}>
+                                  移出借书单
+                                </Button>
+                                <Button
+                                  size="small"
+                                  className="positiveButton"
+                                  onClick={() => borrowBook(pannelData.id)}>
+                                  详情
+                                </Button>
+                              </div>
                             ) : null
                         }
                         {
@@ -399,13 +352,22 @@ const Search = (props) => {
                           //1代表可以借阅
                           functionButtonStyle === 1 ?
                             (
-                              <Button
-                                className="positiveButton"
-                                variant="outlined"
-                                color="primary"
-                                onClick={() => borrowBook(pannelData.id)}>
-                                加入预借书单
-                              </Button>
+                              <div className='book-detail-btn-wrapper'>
+                                <Button
+                                  size="small"
+                                  className="positiveButton"
+                                  variant="outlined"
+                                  color="primary"
+                                  onClick={() => borrowBook(pannelData.id)}>
+                                  加入借书单
+                                </Button>
+                                <Button
+                                  size="small"
+                                  className="positiveButton"
+                                  onClick={() => borrowBook(pannelData.id)}>
+                                  详情
+                                </Button>
+                              </div>
                             ) : null
                         }
                       </div>
@@ -419,7 +381,7 @@ const Search = (props) => {
       <BackTop>
         <div className="back-top-button"><ArrowUpwardIcon/></div>
       </BackTop>
-      <SearchTab visible={visible} setVisible={setVisible} />
+      <SearchTab visible={visible} setVisible={setVisible}/>
     </div>
   )
 }
